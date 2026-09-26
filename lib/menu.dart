f850d23decd9fe5fe7d36040d3ef0ffc25a1489b
cc <cc>
2026-09-26 12:55:53 -0600

    Cambios realizados parte 5

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:proyecto_1/disenios.dart';
 
 class Menu extends StatelessWidget {
   const Menu({super.key});
@@ -7,7 +8,18 @@ class Menu extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        ElevatedButton(onPressed: null, child: Text('Boton #1'),),
+        ElevatedButton(
+          onPressed: (){
+            // mandar a llamar la siguiente pantalla.
+            Navigator.push(
+              context,
+            MaterialPageRoute(
+              builder: (context)=>Disenios()
+            )
+            );
+            
+          }, 
+        child: Text('Boton #1'),),
         ElevatedButton(onPressed: null, child: Text('Boton #2'))
       ],
     );
